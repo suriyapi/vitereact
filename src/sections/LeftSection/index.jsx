346c09dcab765e0b2fa5167760bb182dd5504591
suriyapi <116714861+suriyapi@users.noreply.github.com>
2024-02-18 14:01:58 +0700

    Edit Resp

--- src/sections/LeftSection/index.jsx
+++ src/sections/LeftSection/index.jsx
@@ -5,7 +5,7 @@ import Navbar from "../Navbar";
 const LeftSection = () => {
   return (
     <div className="px-5">
-        <div className='sticky top-14 grid grid-rows-[35%_45%_20%] h-[87vh]'>
+        <div className='sticky top-14 grid gap-y-5 lg:grid-rows-[35%_45%_20%] lg:h-[87vh]'>
             <Header />
             <Navbar />
             <Contact />
