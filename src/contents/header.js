439e48a15919803d85bec0cf9671ff23ec0999fa
suriyapi <116714861+suriyapi@users.noreply.github.com>
2024-02-18 13:30:30 +0700

    edit index.jsx

---
+++ src/contents/header.js
@@ -0,0 +1,9 @@
+import fileName from '../assets/CV.pdf'
+
+export const data = {
+    name: "Suriya Pinitkan",
+    title: "Software Developer · between jobs",
+    caption: "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
+    btnText: "View Resume",
+    link: fileName,
+}
--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -1,16 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import fileResume from '../../assets/CV.pdf'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { data } from '../../contents/header';
+
 const Header = () => {
     return (
         <div className='flex flex-col gap-2'>
-            <div className='text-3xl text-primaryTitle font-semibold'>Suriya Pinitkan</div>
-            <div className='text-primaryAccent font-semibold'>Software Developer · between jobs</div>
-            <div className='text-sm w-5/6'>From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.</div>
+            <div className='text-3xl text-primaryTitle font-semibold'>{data.name}</div>
+            <div className='text-primaryAccent font-semibold'>{data.title}</div>
+            <div className='text-sm w-5/6'>{data.caption}</div>
             <div className='mt-4'>
-              <a href={fileResume} target='_blank'>
+              <a href={data.link} target='_blank'>
                 <span className='rounded-md bg-primaryTitle text-gray-200 py-2 px-2'>
-                  View Resume
+                  {data.btnText}
                   <span className='rotate-90 inline-block ml-2 text-sm'><FontAwesomeIcon className='animate-bounce' icon={faArrowDown} /></span>
                 </span>
               </a>
