54a403a1ce6d723b776dcb76c1be01fa578104be
suriyapi <116714861+suriyapi@users.noreply.github.com>
2024-02-18 14:50:32 +0700

    edit Exp

--- src/sections/Experience/index.jsx
+++ src/sections/Experience/index.jsx
@@ -1,19 +1,20 @@
 import { faGithub, faMedium, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import picExp from "../../assets/exp_site.png";
+import piclittlelemon from "../../assets/littlelemon.png";
 
 const Experience = () => {
     return (
-        <div className='space-y-4'>
+        <div className='space-y-10'>
             <div className='text-primaryAccent font-medium'>Experience</div>
 
             <div className="grid grid-cols-[25%_75%]">
                 <div>
                     <div>
-                      <span className="text-sm">2022-2023</span> 
+                        <span className="text-sm">2022-2023</span>
                     </div>
                     <div>
-                        <img src={picExp} className="w-5/6 rounded-md"/>
+                        <img src={picExp} className="w-5/6 rounded-md border-2 border-primarySubcontent" />
                     </div>
                 </div>
                 <div className="grid gap-y-4">
@@ -27,12 +28,36 @@ const Experience = () => {
                     <div className="flex gap-4 text-sm">
                         <div className="bg-primarySubcontent px-2 py-1 rounded-md hover:text-primaryTitle">React</div>
                         <div className="bg-primarySubcontent px-2 py-1 rounded-md">Taiwind</div>
-                        
+                    </div>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-[25%_75%]">
+                <div>
+                    <div>
+                        <span className="text-sm">2022-2023</span>
+                    </div>
+                    <div>
+                        <img src={piclittlelemon} className="w-5/6 rounded-md border-2 border-primarySubcontent" />
+                    </div>
+                </div>
+                <div className="grid gap-y-4">
+                    <div className="text-primaryAccent">Fullstack · Sport Analysis System</div>
+                    <div className="flex gap-4 text-xl">
+                        <FontAwesomeIcon icon={faGithub} />
+                        <FontAwesomeIcon icon={faMedium} />
+                        <FontAwesomeIcon icon={faYoutube} />
+                    </div>
+                    <div className="text-sm">Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.</div>
+                    <div className="flex gap-4 text-sm">
+                        <div className="bg-primarySubcontent px-2 py-1 rounded-md hover:text-primaryTitle">React</div>
+                        <div className="bg-primarySubcontent px-2 py-1 rounded-md">Taiwind</div>
+
                     </div>
                 </div>
 
             </div>
-            
+
         </div>
     )
 }
